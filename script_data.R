4bb32f309999b7c7c5bd85bc2788e0bd7906dc6c
Sławomir Matuszak <s.matuszak@gmail.com>
2020-02-23 16:07:09 +0100

    plot No 3

--- script_data.R
+++ script_data.R
@@ -32,5 +32,11 @@ consumption <- tbl_df(consumption)
 hist(consumption$Global_active_power, col="red",main="Global Active Power", xlab = "Global Active Power (kilowatts)")
 
 # plot 2
-with(consumption, plot(Date_time, Global_active_power, type="l", ylab = "Global Active Power (kilowatts)", xlab = " "))
+with(consumption, plot(Date_time, Global_active_power, type="l", ylab = "Global Active Power (kilowatts)", xlab = ""))
+
+# plot 3
+with(consumption, plot(Date_time, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+lines(consumption$Date_time, consumption$Sub_metering_2,type="l", col= "red")
+lines(consumption$Date_time, consumption$Sub_metering_3,type="l", col= "blue")
+legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=2, col = c("black", "red", "blue"))
 
