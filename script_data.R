c1001838e5e5ef895a8c23c62938cb7ba9afce42
Sławomir Matuszak <s.matuszak@gmail.com>
2020-02-23 16:12:18 +0100

    plot No 4

--- script_data.R
+++ script_data.R
@@ -40,3 +40,13 @@ lines(consumption$Date_time, consumption$Sub_metering_2,type="l", col= "red")
 lines(consumption$Date_time, consumption$Sub_metering_3,type="l", col= "blue")
 legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=2, col = c("black", "red", "blue"))
 
+# plot 4
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+with(consumption, plot(Date_time, Global_active_power, type="l", xlab="", ylab="Global Active Power"))
+with(consumption, plot(Date_time, Voltage, type = "l", xlab="datetime", ylab="Voltage"))
+with(consumption, plot(Date_time, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+lines(consumption$Date_time, consumption$Sub_metering_2,type="l", col= "red")
+lines(consumption$Date_time, consumption$Sub_metering_3,type="l", col= "blue")
+legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=2, col = c("black", "red", "blue"))
+with(consumption, plot(Date_time, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power"))
